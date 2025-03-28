3315bac119b26a5e3ddfea176f0917d8c9b40545
Tygrett <wendy.tygrett@cgi.com>
2025-03-27 19:01:40 -0500

    Added button variants; added more named colors in Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
  export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'navy-blue': '#1B0067',
+        'navy-blue-desat': '#3F005C',
+        'eggplant': '#67004B',
+        'eggplant-desat': '#7A0040',
+        'blue-violet': '#4F0067',
+        'blue-violet-desat': '#6C0061',
+        'wine': '#670018',
+        'wine-desat': '#7A0029',
+        'emerald': '#00523F',
+        'emerald-desat': '#3B5A4E',
+        'gold': '#e5b243'
+      },
+    },
   },
   plugins: [],
 }
